51ceeeda412188433681b506b092eaf48d461ad7
Adish Narayan <adishnarayan123@gmail.com>
2023-10-12 12:28:57 +0400

    practice5

--- Chapter 1- Getting Started with Python/Exercises/practice5.py
+++ Chapter 1- Getting Started with Python/Exercises/practice5.py
@@ -5,5 +5,5 @@ string2 ="world"
 string3 ="!"
 string4 ="how are you?"
 # concantenate the string and print the output as one string 
-output_string = string1 + string2 + string3 + string4 
+output_string = string1  + string2 + string3 + string4 
 print(output_string)
