882f51513635c507eeb78051736cfb619f31d41d
Adish Narayan <adishnarayan123@gmail.com>
2023-10-17 00:28:53 +0400

    exercisess

---
+++ Chapter 2- Variables & Comments/Exercises/Exercises2.py
@@ -0,0 +1,2 @@
+#write quote
+print(f'Mahatma Gandhi once said,"My religion is based on truth and non-violence Truth is my God".')
---
+++ Chapter 2- Variables & Comments/Exercises/Exercises4.py
@@ -0,0 +1,4 @@
+# Exercise 4: Favorite Number :ballot_box_with_check:
+f_number= 20
+message="My favourite number is:" + str(f_number)
+print(message)
