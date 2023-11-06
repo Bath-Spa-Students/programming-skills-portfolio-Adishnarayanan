bd1d2e2171ade638ab1cc1ad12e2c0766ccb2772
Adish Narayan <adishnarayan123@gmail.com>
2023-11-06 10:50:18 +0400

    exercises

---
+++ Chapter 5- Dictionaries/pratice.py
@@ -0,0 +1,14 @@
+#tuples are
+
+tup=(1,2,3)
+print(type(tup),tup)
+
+#to add some value in dictionary 
+
+example={'color' :'red' ,
+         'fruit' : 'apple' ,
+         'place': 'UAE'}
+print(example)
+
+dictionary={'name' : 'Adish Narayan' ,
+            'place':'RAK'}
